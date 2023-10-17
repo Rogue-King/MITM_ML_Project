9bf7b56b4d52c9abb2beb71e13b98b15f4a60020
Miguel Muniz <miguelmuniz@nevada.unr.edu>
2023-10-17 11:57:49 -0700

    edits

--- arpPreprocess.py
+++ arpPreprocess.py
@@ -1,10 +1,7 @@
 import numpy as np
 from scapy.all import *
 
-def extract_features(packet):
-    # Extract relevant features from the packet (customize based on your use case)
-    features = [packet[IP].src, packet[IP].dst, packet[IP].proto, len(packet)]
-    return featuresdef arp_packet_handler(packet):
+def arp_packet_handler(packet):
     if ARP in packet:
         arp_packet = packet[ARP]
         if arp_packet.op == 1:  # ARP Request
@@ -12,3 +9,4 @@ def extract_features(packet):
 
 # Sniff ARP packets on a specific interface (e.g., "eth0") for a certain number of packets (count) and call arp_packet_handler for each ARP packet.
 sniff(iface="eth0", count=10, prn=arp_packet_handler, filter="arp")
+
