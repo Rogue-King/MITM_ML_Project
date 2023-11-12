f424290cd777c02e45a0fdf9abcfe48d12c1fd6a
Miguel Muniz <miguelmuniz@nevada.unr.edu>
2023-11-11 18:17:29 -0800

    updated

--- RNN.py
+++ RNN.py
@@ -1,11 +1,24 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, SimpleRNN
-from arpPreprocess import extractFeatures 
+from arpPreprocess import *
 
 # Define the model architecture
 model = Sequential()
 model.add(SimpleRNN(units=64, input_shape=(10, 5)))
 model.add(Dense(units=3, activation='softmax'))
+
+# Assuming you have features and labels from your dataset
+# Replace this with your actual data loading and preprocessing
+x, y = extractFeatures()
+
+# One-hot encode the labels
+encoder = OneHotEncoder()
+y_one_hot = encoder.fit_transform(y.reshape(-1, 1)).toarray()
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y_one_hot, test_size=0.2, random_state=42)
+
+
 
 # Compile the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
