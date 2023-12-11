e22cfc2352accd5e1dec9c809eff0771468286b1
christian-culanag <christianculanag@nevada.unr.edu>
2023-12-10 22:08:31 -0800

    Created Logic for Preprocessing Data

--- webapp/controllers/upload_controller.py
+++ webapp/controllers/upload_controller.py
@@ -4,6 +4,7 @@ from scapy.all import PcapReader, ARP
 import csv
 from werkzeug.utils import secure_filename
 
+from webapp.controllers.ml_controller import update_csv_with_predictions
 
 ALLOWED_EXTENSIONS = {".pcap", ".PCAP"}
 
@@ -27,30 +28,39 @@ def file_upload_success():
         flash('File is not a recognized format. Please upload .pcap files only')
         return False
     
-    session['pcap_filepath'] = f'webapp/file_uploads/{secure_filename(pcap_file.filename).replace(".pcap", ".csv")}'
-        
-    create_csv(pcap_file)
+    session['pcap_csv_filepath'] = f'webapp/file_uploads/{secure_filename(pcap_file.filename).replace(".pcap", ".csv")}'
+    session['processed_pcap_csv_filepath'] = f'webapp/file_uploads/{secure_filename(pcap_file.filename).replace(".pcap", "_process.csv")}'
+
+    create_csvs(pcap_file)
+
+    update_csv_with_predictions()
 
     flash(f'Uploaded {secure_filename(pcap_file.filename)} successfully')
     
     return True
 
-def create_csv(file):
-    with open(session.get('pcap_filepath'), 'w', newline="\n") as csv_file:
+def create_csvs(file):
+    with open(session.get('pcap_csv_filepath'), 'w', newline="\n") as csv_file, open(session.get('processed_pcap_csv_filepath'), 'w', newline="\n") as preprocessed_csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(['Packet #','Source IP', 'Destination IP', 'Source MAC', 'Destination MAC', 'Payload'])
 
-        parse_pcap_file(file, csv_writer)
+        preprocess_writer = csv.writer(preprocessed_csv_file)
+        preprocess_writer.writerow(['Source IP', 'Destination IP', 'Source MAC', 'Destination MAC', 'Opcode'])
 
-def parse_pcap_file(file, writer):
+        parse_pcap_file(file, csv_writer, preprocess_writer)
+
+def parse_pcap_file(file, csv_writer, preprocess_writer):
     packet_number = 1;
 
     for packet in PcapReader(file):
         if (packet.haslayer(ARP)):
             packet_info = [packet_number, packet.psrc, packet.pdst, packet.src, packet.dst, determine_arp_payload(packet)]
-            packet_number += 1;
+            preprocess_packet_info = [convert_ip_to_dec(packet.psrc), convert_ip_to_dec(packet.pdst), convert_mac_to_dec(packet.src), convert_mac_to_dec(packet.dst), packet.op]
 
-            writer.writerow(packet_info)
+            packet_number += 1;
+                        
+            csv_writer.writerow(packet_info)
+            preprocess_writer.writerow(preprocess_packet_info)
 
 def determine_arp_payload(packet):
     operation = ARP_MAPPINGS.get(packet.op)
@@ -60,3 +70,13 @@ def determine_arp_payload(packet):
     
     if operation == "IS-AT":
         return f"{packet.psrc} {operation} {packet.hwsrc}"
+    
+def convert_ip_to_dec(ip_address):
+    octets = ip_address.split('.')
+
+    return (int(octets[0]) << 24) + (int(octets[1]) << 16) + (int(octets[2]) << 8) + int(octets[3])
+
+def convert_mac_to_dec(mac_address):
+    octets = mac_address.split(':')
+
+    return (int(octets[0], 16) << 40) + (int(octets[1], 16) << 32) + (int(octets[2], 16) << 24) + (int(octets[3], 16) << 16) + (int(octets[4], 16) << 8) + int(octets[5], 16)
--- webapp/routes/packet_viewer.py
+++ webapp/routes/packet_viewer.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, session, redirect, url_for
-from webapp.controllers.pcap_viewer_controller import *
+from webapp.controllers.pcap_viewer_controller import get_table_segment
+
 import math
 
 packet_viewer = Blueprint("packet_viewer", __name__, template_folder="../views/templates/", static_folder="../views/static/")
@@ -8,11 +9,11 @@ packet_viewer = Blueprint("packet_viewer", __name__, template_folder="../views/t
 @packet_viewer.route('/', defaults={'page': 1})
 @packet_viewer.route('/<int:page>')
 def default_page(page):
-    pcap_filepath = session.get('pcap_filepath', None)
+    pcap_filepath = session.get('pcap_csv_filepath', None)
 
     if (pcap_filepath == None):
         return redirect('/')
-    
+
     packets_per_page = 25
 
     # Get the total of packets by counting the total number of lines (minus the column headers and the trailing new line)
