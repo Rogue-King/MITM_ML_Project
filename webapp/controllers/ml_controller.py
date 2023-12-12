e077a7297cf82cb6c3ab7513336f68fcc58dc3c9
christian-culanag <christianculanag@nevada.unr.edu>
2023-12-11 19:26:20 -0800

    Removed Statistics

--- webapp/controllers/ml_controller.py
+++ webapp/controllers/ml_controller.py
@@ -32,23 +32,3 @@ def update_csv_with_predictions():
     packet_table_csv['Label'] = pd.Series(predictions_array, index=packet_table_csv.index)
 
     packet_table_csv.to_csv(session.get('pcap_csv_filepath'), index=False)
-
-    current_dir = os.getcwd()
-    #new_directory = os.path.abspath(os.path.join(current_dir, '..', '..'))
-    # print(new_directory + '/Scripts/Data_Label_Script/L_CSV/Melchior_training_arp_data_min.csv')
-    label_actual = pd.read_csv(current_dir + '/Scripts/Data_Label_Script/L_CSV/Melchior_training_arp_data_min.csv')['Label'].tolist()
-    tp, fp, fn, tn = 0, 0, 0, 0,
-    for i in predictions_array:
-        if (predictions_array[i] and label_actual[i] == 1):
-            tp += 1
-        elif (predictions_array[i] and label_actual[i] == 0):
-            fp += 1
-        elif (not predictions_array[i] and label_actual[i] == 1):
-            fn += 1
-        else:
-            print(predictions_array) 
-            print(label_actual)
-            tn += 1
-        i += 1
-
-    print(f'Statistics: tp: {tp} fp: {fp} fn: {fn} tn: {tn}')
