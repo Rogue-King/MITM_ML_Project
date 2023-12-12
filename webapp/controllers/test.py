ee00767c16c02f376f77268a7fcbfebbb69c4615
christian-culanag <christianculanag@nevada.unr.edu>
2023-12-11 22:58:37 -0800

    Removed unnecessary files

--- webapp/controllers/test.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-current_dir = os.getcwd()
-new_directory = os.path.abspath(os.path.join(current_dir, '..', '..'))
-print(f'{new_directory}' + '/scripts/Data_Label_Script/L_CSV/Melchior_training_arp_data_min.csv')
